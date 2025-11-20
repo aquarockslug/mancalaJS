0ac5e14842ce01c2b3b3efd7f11296b748bc1610
aqualava <aqua.rock.slug@gmail.com>
2025-11-20 11:21:33 -0600

    itch

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
-const POCKETPOS = vec2(-10.4, -2.25);
-const POCKETSIZE = vec2(2.6);
+// mancalaJS by aquarock
 
-const BOARDWIDTH = 8;
-const BOARDHEIGHT = 2;
+// Array that records all of the moves made in the game
+boardMoves = [];
+
+Pocket = (index, count, home) => ({ index, count, home });
 
 const SANDRED = new Color(0.78, 0.28, 0.03);
 const SANDLIGHTBROWN = new Color(0.97, 0.88, 0.63);
@@ -12,46 +13,50 @@ const INITMARBLECOUNT = 3;
 const MARBLECOLOR = SANDRED;
 const MARBLESIZE = 0.35;
 
-boardMoves = []; // Array to track all moves made in the game
-moving = false;
+const POCKETPOS = vec2(-10.4, -2.25);
+const POCKETSIZE = vec2(2.6);
 
-Pocket = (index, count, home) => ({ index, count, home });
+const BOARDWIDTH = 8;
+const BOARDHEIGHT = 2;
+
+const BUTTONPOS = screenToWorld(vec2( -310, -155 ));
+const BUTTONSIZE = 1.25;
 
 function gameInit() {
 	setCanvasFixedSize(vec2(640, 360));
 	cameraScale -= 1.5;
-
 	initBoard();
 }
 
 function isMouseOverValidPocket(pos) {
 	return (
 		mousePos.distance(pos.value) < POCKETSIZE.x / 2 &&
-		pos.index !== 0 &&
+		pos.index > 0 &&
 		pos.index !== 7 &&
 		pos.index !== 16
 	);
 }
 
+function resetButton() {
+	return !(boardMoves.length - 2) ? null : mousePos.distance(BUTTONPOS) < POCKETSIZE.x / 2
+}
+
 function gameUpdate() {
 	if (!mouseWasPressed(0)) return;
+	if (resetButton()) {
+		boardMoves.pop()
+	}
 	for (pos of positions) {
 		if (isMouseOverValidPocket(pos)) playMove(pos.index);
 	}
 }
 
-function gameUpdatePost() {}
-
 // ==================== BOARD LOGIC ====================
 
 // iterates through all pockets and applies the move function
 function* moveMarbles(state, move) {
-	for (
-		let pocketIndex = 0;
-		pocketIndex < BOARDHEIGHT * BOARDWIDTH - 2;
-		pocketIndex++
-	)
-		yield move(pocketIndex, state[pocketIndex], state);
+	for (let i = 0; i < BOARDHEIGHT * BOARDWIDTH - 2; i++)
+		yield move(i, state[i], state);
 }
 
 // returns an array representing the current marble count in each pocket
@@ -65,8 +70,8 @@ function getBoardState() {
 function playMove(startingPocketIndex) {
 	currState = getBoardState();
 	move = (i, m, state) => {
-		start = state[startingPocketIndex];
-		crossing = start.index % 15 > (start.index + start.count) % 14;
+		let start = state[startingPocketIndex];
+		let crossing = start.index % 15 > (start.index + start.count) % 14;
 
 		// find the indicies that need to updated
 		if (
@@ -82,7 +87,7 @@ function playMove(startingPocketIndex) {
 	return Array.from(moveMarbles(currState, move));
 }
 
-// use moves that set up the board
+// create moves that set up the board
 function initBoard() {
 	boardMoves = [
 		(i, _) => Pocket(i, null, false),
@@ -136,7 +141,6 @@ function drawHomePocket(pos, count) {
 	);
 }
 
-// draw marbles in a circle
 function drawMarbles(pos, count) {
 	getOffset = (i) =>
 		i < 7
@@ -147,16 +151,25 @@ function drawMarbles(pos, count) {
 		drawCircle(pos.add(getOffset(i)), MARBLESIZE, MARBLECOLOR);
 }
 
-function gameRender() {
-	// draw background layers
+function drawBackground() {
 	drawRect(vec2(0), vec2(32), SANDLIGHTBROWN);
 	drawRect(vec2(0, 0.25), vec2(32, 7), SANDRED);
 	drawRect(vec2(0, -2.425), vec2(32, 0.05), BLACK);
-	// drawRect(vec2(-10.45, 0.25), vec2(0.05, 7), BLACK);
+}
 
-	// get current game state and pocket positions
-	state = getBoardState();
-	positions = getPocketPos();
+function gameRender() {
+	let [state, positions] = [getBoardState(), getPocketPos()]
+
+	drawBackground()
+	drawCircle(BUTTONPOS, BUTTONSIZE, SANDRED);
+	drawTextScreen(
+		"\u20D4",
+		worldToScreen(BUTTONPOS.add(vec2(0.35, -0.5))),
+		32,
+		BLACK,
+		2,
+		BLACK,
+	);
 
 	// draw each pocket and its marbles
 	for (pos of positions) {
@@ -165,7 +178,6 @@ function gameRender() {
 		pocket = state[pos.index];
 
 		// draw pocket
-		// drawCircle(pos.value, 2.5, BLACK);
 		if (pocket.index === 0 || pocket.index === 7)
 			drawHomePocket(pos.value, pocket.count);
 		else {
@@ -176,5 +188,3 @@ function gameRender() {
 		drawMarbles(pos.value, pocket.count);
 	}
 }
-
-function gameRenderPost() {}
