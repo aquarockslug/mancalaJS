f5dfc2cbee8bd38023cb56572c661a24fb7c612f
aqualava <aqua.rock.slug@gmail.com>
2025-11-19 20:10:41 -0600

    consts and visuals

--- javascript/game.js
+++ javascript/game.js
@@ -1,10 +1,54 @@
-const BOARDPOS = vec2(-4, 0);
-const BOARDSCALE = vec2(1);
+const POCKETPOS = vec2(-5.6, -1.25);
+const POCKETSCALE = vec2(1.25);
+const BOARDSIZE = vec2(12.5, 4);
+const INITPIECES = 3;
 
-// the state of the game is stored with a sequence of move functions
 let boardMoves = [];
 let pocket = (i, m) => ({ index: i, count: m });
 
+function gameInit() {
+	setCanvasFixedSize(settings.screenResolution);
+	[width, height] = [9, 2];
+
+	// init board
+	playMove([], (i, m) => pocket(i, INITPIECES));
+}
+
+function gameStart() {
+	gamestate = "playing";
+}
+
+function gameUpdate() {
+	// if (state != "playing") return;
+}
+
+function gameUpdatePost() {}
+
+// BOARD
+function* moveMarbles(state, move) {
+	for (let pocketIndex = 0; pocketIndex <= height * width; pocketIndex++)
+		yield move(pocketIndex, state[pocketIndex]);
+}
+
+function getBoardState() {
+	// get the state of the board by playing every move
+	let calcBoard = (acc, curr) => Array.from(moveMarbles(acc, curr));
+	return boardMoves.reduce(calcBoard, []);
+}
+
+function playMove(state, move) {
+	boardMoves.push(move);
+	return Array.from(moveMarbles(state, move));
+}
+
+// MOVES
+function testMove() {
+	let firstMove = (i, m) => (i > 10 ? pocket(i, m.count + 3) : pocket(i, 0));
+
+	return playMove(getBoardState(), firstMove);
+}
+
+// RENDER
 function getPocketPos() {
 	let positions = [];
 	let i = 0;
@@ -13,80 +57,29 @@ function getPocketPos() {
 			i++;
 			positions.push({
 				index: i,
-				pos: vec2(x, y).multiply(BOARDSCALE).add(BOARDPOS),
+				pos: vec2(x, y).multiply(POCKETSCALE).add(POCKETPOS),
 			});
 		}
 	}
 	return positions;
 }
 
-function initBoardState() {
-	let state = [];
-	for (let i = 0; i <= width * height; i++) state.push({ index: i, count: 3 });
-	return state;
-}
-
-function* moveMarbles(state, move) {
-	for (let pocketIndex = 0; pocketIndex <= height * width; pocketIndex++) {
-		yield move(pocketIndex, state[pocketIndex]);
-	}
-}
-
-function getBoardState() {
-	// moveMarbles(moveMarble(initBoardState(), move1), move2)
-	let calcMove = (acc, curr) => Array.from(moveMarbles(acc, curr));
-	return boardMoves.reduce(calcMove, initBoardState());
-}
-
-function playMove(state, move) {
-	boardMoves.push(move);
-	return Array.from(moveMarbles(state, move));
-}
-
-function gameInit() {
-	setCanvasFixedSize(settings.screenResolution);
-	[width, height] = [9, 2];
-
-	boardMoves = [];
-
-	// Initial move logic
-	let state = playMove(initBoardState(), (i, m) => pocket(i, 3));
-}
-
-function testMove() {
-	let firstMove = (i, m) =>
-		i > 10 ? pocket(i, m.count + 3) : pocket(i, 0);
-
-	return playMove(getBoardState(), firstMove);
-}
-
-function gameStart() {
-	gamestate = "playing";
-}
-
-function gameOver() {}
-
-function gameUpdate() {
-	// if (state != "playing") return;
-}
-
-function gameUpdatePost() {}
-
 function gameRender() {
-	// Background
-	drawRect(vec2(0), vec2(16), (color = GRAY), (angle = 0));
+	// background
+	drawRect(vec2(0), vec2(32), (color = WHITE), (angle = 0));
+	drawRect(vec2(0), BOARDSIZE, (color = GRAY), (angle = 0));
 
 	let state = getBoardState();
-
 	let positions = getPocketPos();
 	for (let p of positions) {
-		drawRect(p.pos, vec2(0.9), WHITE);
+		// pocket background
+		drawCircle(p.pos, 1.1, WHITE);
 
-		let marble = state[p.index];
-		// TODO for each marble.count
-		if (marble) {
-			drawRect(p.pos, vec2(0.2), marble.count > 0 ? RED : WHITE);
-		}
+		let pocket = state[p.index];
+		// TODO render each marble in a honeycomb shape
+		// split a circles circumference into 5 parts
+		for (let i = 0; i < pocket.count; i++)
+			drawRect(p.pos, vec2(0.2), RED);
 	}
 }
 
